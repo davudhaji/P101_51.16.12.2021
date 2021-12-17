2d8eeb569803db618776ce657995ac9aea1efdc0
Haji <tu201806253@aztu.local>
2021-12-17 12:52:53 +0400

    first

---
+++ WebApplication5/Models/article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class article
+    {
+        [Key]
+        public int id { get; set; }
+
+        [MaxLength(250)]
+        public int title { get; set; }
+        [MaxLength(250)]
+        public string image { get; set; }
+        public string content { get; set; }
+        public int views { get; set; }
+        public int like { get; set; }
+        public int dislike { get; set; }
+    
+        public DateTime date { get; set; }
+
+    }
+
+}
---
+++ WebApplication5/Models/category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class category
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(150)]
+        public string name { get; set; }
+
+        List<article> articles;
+        
+    }
+}
